6bc0c91aa2d297e9834b9054947dd96d227daad6
Miranda Durham <mdurham@Miranda-Durhams-iMac.local>
2015-08-07 17:16:20 -0600

    Initial Commit

---
+++ The Daily Charger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  The Daily Charger
+//
+//  Created by Miranda Durham on 8/7/15.
+//  Copyright (c) 2015 dOOP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
